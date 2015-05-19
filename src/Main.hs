e892723cd7c51a8066465881da3e1c7a8025dd6a
Konstantin Zudov <konstantin@anche.no>
2015-05-19 20:07:34 +0300

    Escaping

--- src/Main.hs
+++ src/Main.hs
@@ -1,28 +1,50 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import Lambdabot.Pointful
 import Pointfree
 
 import Control.Monad.Trans (liftIO)
+import Data.Text (Text)
+import qualified Data.Text as T
+
 import GHCJS.DOM (webViewGetDomDocument, runWebGUI)
 import GHCJS.DOM.Document (documentGetElementById)
 import GHCJS.DOM.HTMLElement
 import GHCJS.DOM.Element
 import GHCJS.DOM.HTMLInputElement
-import Data.List (intercalate)
 
 main :: IO ()
 main = runWebGUI $ \webView -> do
     Just doc         <- webViewGetDomDocument webView
-    Just input       <- documentGetElementById doc "input"
-    Just pointfreeEl <- documentGetElementById doc "pointfree"
-    Just pointfulEl  <- documentGetElementById doc "pointful"
+    Just input       <- documentGetElementById doc ("input" :: Text)
+    Just pointfreeEl <- documentGetElementById doc ("pointfree" :: Text)
+    Just pointfulEl  <- documentGetElementById doc ("pointful" :: Text)
 
     let handler = liftIO $ do
             val <- htmlInputElementGetValue $ castToHTMLInputElement input
-            htmlElementSetInnerHTML (castToHTMLElement pointfreeEl) $ intercalate "<br>" $ pointfree val
-            htmlElementSetInnerHTML (castToHTMLElement pointfulEl) $ pointful val
+            htmlElementSetInnerHTML (castToHTMLElement pointfreeEl) $ evalPointfree val
+            htmlElementSetInnerHTML (castToHTMLElement pointfulEl) $ evalPointful val
 
     _ <- elementOnkeyup input $ handler
     _ <- elementOnkeydown input $ handler
     return ()
+
+evalPointfree :: String -> Text
+evalPointfree = T.intercalate "<br>" . map (escape . T.pack) . pointfree
+
+evalPointful :: String -> Text
+evalPointful input = escape $ ignoreErrors $ T.pack $ pointful $ input
+  where
+    ignoreErrors output
+      | any (`T.isPrefixOf` output) ["Error:", "<unknown>.hs"] = ""
+      | ";" `T.isSuffixOf` output && not (";" `T.isSuffixOf` T.pack input) = T.init output
+      | otherwise = output
+
+escape :: Text -> Text
+escape = T.replace "/" "&#x2F"
+       . T.replace "'" "&#39;"
+       . T.replace "\"" "&quot;"
+       . T.replace ">" "&gt;"
+       . T.replace "<" "&lt;"
+       . T.replace "&" "&amp;"
